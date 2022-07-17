06e4131e0431705a7a07c9abd1c5c86b1ae662a9
arisinenis <enisarisin@gmail.com>
2022-07-17 14:13:19 +0300

    Repositories created.

---
+++ Core/ECommerceAPI.Application/Repositories/IReadRepository.cs
@@ -0,0 +1,20 @@
+﻿using ECommerceAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Application.Repositories
+{
+    // Queryler için kullanılacak. -GetAll gibi (Data manipulation için değil yalnızca datayı okumak için kullanılanlar)
+    public interface IReadRepository<T> : IRepository<T>
+        where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(string id);
+        IQueryable <T> GetAll();
+        IQueryable <T> GetWhere(Expression<Func<T, bool>> expression);
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> expression);
+    }
+}
--- Infrastructure/ECommerceAPI.Persistence/ServiceRegistration.cs
+++ Infrastructure/ECommerceAPI.Persistence/ServiceRegistration.cs
@@ -1,4 +1,6 @@
-﻿using ECommerceAPI.Persistence.Contexts;
+﻿using ECommerceAPI.Application.Repositories;
+using ECommerceAPI.Persistence.Contexts;
+using ECommerceAPI.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,7 +16,17 @@ namespace ECommerceAPI.Persistence
     {
         public static void AddPersistenceService(this IServiceCollection services, IConfiguration config)
         {
-            services.AddDbContext<ECommerceAPIDbContext>(options => options.UseSqlServer(config.GetConnectionString("ECommerceDB")));
+            // AddDbContextin ServiceLifetime'ı default olarak scoped'tur. Dependecy inj. yapılan nesne her istediğinde bir tane oluşturur ve onu bize verir. Burada default singleton'a çektik. bir tane oluşturulur ve program çalışırken hep o kullanılır.
+            services.AddDbContext<ECommerceAPIDbContext>(options => options.UseSqlServer(config.GetConnectionString("ECommerceDB")), ServiceLifetime.Singleton);
+
+            services.AddSingleton<ICustomerReadRepository, CustomerReadRepository>();
+            services.AddSingleton<ICustomerWriteRepository, CustomerWriteRepository>();
+            services.AddSingleton<IOrderWriteRepository, OrderWriteRepository>();
+            services.AddSingleton<IOrderReadRepository, OrderReadRepository>();
+            services.AddSingleton<IProductWriteRepository, ProductWriteRepository>();
+            services.AddSingleton<IProductReadRepository, ProductReadRepository>();
+            services.AddSingleton<ICategoryWriteRepository, CategoryWriteRepository>();
+            services.AddSingleton<ICategoryReadRepository, CategoryReadRepository>();
         }
     }
 }
